802d39958a95604d1befde82002e9745f4740728
Will Mayger <wmayger@outlook.com>
2015-08-15 12:01:16 +0100

    added service agreement method inside of class

--- contract_template.py
+++ contract_template.py
@@ -6,7 +6,7 @@ class ContractGenerator:
 
     def __init__(self, client_name, client_address, client_town, client_country, client_postcode,
                  service_provider_name, service_provider_address, service_provider_town,
-                 service_provider_country, service_provider_postcode):
+                 service_provider_country, service_provider_postcode, service_provided):
 
         # date time
         now = datetime.now()
@@ -32,6 +32,7 @@ class ContractGenerator:
         self.service_provider_town = service_provider_town
         self.service_provider_country = service_provider_country
         self.service_provider_postcode = service_provider_postcode
+        self.service_provided = service_provided
 
         # new instance of Document
         self.contract = Document()
@@ -43,12 +44,13 @@ class ContractGenerator:
                 self.date, self.month, self.year
             ), 1)
 
-    def agreement(self):
+    # first part of the contract document adding the addresses
+    def client_and_service_info(self):
 
         self.contract.add_heading(
             'BETWEEN', 2)
 
-        p = self.contract.add_paragraph(
+        self.contract.add_paragraph(
             '{0} of {1}, {2}, {3},\n {4} \n (the "Customer")'.format(
                 self.client_name, self.client_address, self.client_town, self.client_country,
                 self.client_postcode
@@ -64,13 +66,60 @@ class ContractGenerator:
                 self.service_provider_postcode
             ))
 
+    def background(self):
+        self.contract.add_heading(
+            'BACKGROUND:', 2)
+
+        self.contract.add_paragraph(
+            '1. The Customer is of the opinion that the Service Provider has the necessary qualifications,' +
+            ' experience and abilities to provide services to the Customer.'
+            )
+        self.contract.add_paragraph(
+            '2. The Service Provider is agreeable to providing such services to the Customer on the terms' +
+            ' and conditions set out in this Agreement.'
+            )
+
+    def in_consideration_of(self):
+
+        self.contract.add_heading(
+            'IN CONSIDERATION OF:', 2)
+
+        self.contract.add_paragraph(
+            'IN CONSIDERATION OF the matters described above and of the mutual benefits and obligations set' +
+            ' forth in this Agreement, the receipt and sufficiency of which consideration is hereby acknowledged,' +
+            ' the Customer and the Service Provider (individually the "Party" and collectively the "Parties" to ' +
+            'this Agreement) agree as follows:'
+            )
+
+        self.contract.add_heading(
+            '1. Services Provided\n',
+            6)
+
+        self.contract.add_paragraph(
+            '2. The Customer hereby agrees to engage the Service Provider to provide the Customer with services'
+            ' (the "Services") consisting of:'
+            )
+
+        self.contract.add_paragraph(
+            '  2.1 ' + self.service_provided
+        )
+
+        self.contract.add_paragraph(
+            '3. The Services will also include any other tasks which the Parties may agree on.' +
+            ' The Service Provider hereby agrees to provide such Services to the Customer.'
+        )
+
     def save(self):
 
         self.contract.save('contract.docx')
 
 
 cg = ContractGenerator("Mr Example", "93 london drive", "camden", "london", "LON DON",
-                       "Mr Company Example", "93 Company drive", "Regents Place", "London", "LON DON")
+                       "Mr Company Example", "93 Company drive", "Regents Place", "London", "LON DON",
+                       "Responsive Website Development with up to date responsive devices to work across all devices."
+                       )
 cg.heading()
-cg.agreement()
+cg.client_and_service_info()
+cg.background()
+cg.in_consideration_of()
 cg.save()
